ad64e0cc9ead2c302a88c3e877f13993c712d3c2
Jatinnagarwal5 <164998572+Jatinnagarwal5@users.noreply.github.com>
2024-06-10 18:22:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Portfolio
+GitHub-https://github.com/Jatinnagarwal5
+
+Linkedin-https://www.linkedin.com/in/jatin-nagarwal
+
+Instagram - https://www.instagram.com/necko_scorpion?igsh=eXBod3l3NDd0aTZz
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Engineer Student", "Programmer", "Freelancer", "Web Developer",],
+    typeSpeed : 10,
+    backSpeed : 200,
+    backDelay : 1000,
+    loop : true
+});
